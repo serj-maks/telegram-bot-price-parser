250bc6ddb74c55779cc54af527b33de3c5128a7b
Sergey Maksimov(work pc) <sergeymaksimov1993@gmail.com>
2022-05-30 17:38:19 +0300

    init commit

---
+++ src/main/java/com/bot/Bot.java
@@ -0,0 +1,64 @@
+package com.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 
+ * This example bot is an echo bot that just repeats the messages sent to him
+ *
+ */
+@Component
+class Bot extends TelegramLongPollingBot {
+	private static final Logger logger = LoggerFactory.getLogger(Bot.class);
+	private final String token;
+	private final String username;
+
+	Bot(@Value("${bot.token}") String token, @Value("${bot.username}") String username) {
+		this.token = token;
+		this.username = username;
+	}
+	
+	@Override
+	public String getBotToken() {
+		return token;
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return username;
+	}
+	
+	@Override
+	public void onUpdateReceived(Update update) {
+		if (update.hasMessage()) {
+			Message message = update.getMessage();
+			SendMessage response = new SendMessage();
+			Long chatId = message.getChatId();
+			response.setChatId(String.valueOf(chatId));
+			String text = message.getText();
+			response.setText(text);
+			try {
+				execute(response);
+				logger.info("Sent message \"{}\" to {}", text, chatId);
+			} catch (TelegramApiException e) {
+				logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+			}
+		}
+	}
+
+	@PostConstruct
+	public void start() {
+		logger.info("username: {}, token: {}", username, token);
+	}
+
+}
