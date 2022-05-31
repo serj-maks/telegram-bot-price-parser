b727b851e272eb6d7f72941ae88f89765821c6b7
Sergey Maksimov(work pc) <sergeymaksimov1993@gmail.com>
2022-05-31 17:31:09 +0300

    refactoring: tabs, void lines

--- src/main/java/com/bot/Bot.java
+++ src/main/java/com/bot/Bot.java
@@ -12,53 +12,48 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 
-/**
- * 
- * This example bot is an echo bot that just repeats the messages sent to him
- *
- */
 @Component
 class Bot extends TelegramLongPollingBot {
-	private static final Logger logger = LoggerFactory.getLogger(Bot.class);
-	private final String token;
-	private final String username;
+    private static final Logger logger = LoggerFactory.getLogger(Bot.class);
+    private final String token;
+    private final String username;
 
-	Bot(@Value("${bot.token}") String token, @Value("${bot.username}") String username) {
-		this.token = token;
-		this.username = username;
-	}
-	
-	@Override
-	public String getBotToken() {
-		return token;
-	}
-	
-	@Override
-	public String getBotUsername() {
-		return username;
-	}
-	
-	@Override
-	public void onUpdateReceived(Update update) {
-		if (update.hasMessage()) {
-			Message message = update.getMessage();
-			SendMessage response = new SendMessage();
-			Long chatId = message.getChatId();
-			response.setChatId(String.valueOf(chatId));
-			String text = message.getText();
-			response.setText(text);
-			try {
-				execute(response);
-				logger.info("Sent message \"{}\" to {}", text, chatId);
-			} catch (TelegramApiException e) {
-				logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
-			}
-		}
-	}
+    Bot(@Value("${bot.token}") String token, @Value("${bot.username}") String username) {
+        this.token = token;
+        this.username = username;
+    }
 
-	@PostConstruct
-	public void start() {
-		logger.info("username: {}, token: {}", username, token);
-	}
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(String.valueOf(chatId));
+            String text = message.getText();
+            response.setText(text);
+            try {
+                execute(response);
+                logger.info("Sent message \"{}\" to {}", text, chatId);
+            } catch (TelegramApiException e) {
+                logger.error("Failed to send message \"{}\" to {} due to error: {}", text, chatId, e.getMessage());
+            }
+        }
+    }
+
+    @PostConstruct
+    public void start() {
+        logger.info("username: {}, token: {}", username, token);
+    }
 
 }
--- src/main/java/com/bot/BotApplication.java
+++ src/main/java/com/bot/BotApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BotApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BotApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
 }
